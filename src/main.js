7dbb75d0d8194e38832f6ea52127f3f7a107c182
hakimihamzan <hakimihamzan20@gmail.com>
2021-08-31 19:20:51 +0800

    finished side nav bar with awesome

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-console.log("hi");
+let allSideButton = document.querySelectorAll(".side-btn");
+
+for (let i = 0; i < allSideButton.length; i++) {
+  let button = allSideButton[i];
+  button.addEventListener("click", () => {
+    removeClassNameFromNodes(allSideButton, "select");
+    button.classList.add("select");
+  });
+}
+
+function removeClassNameFromNodes(allItems, classNameToRemove) {
+  for (let i = 0; i < allItems.length; i++) {
+    allItems[i].classList.remove(classNameToRemove);
+  }
+}
